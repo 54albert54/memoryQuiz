1d52b7a31938012df1d5ffb66e06a820f0e17d79
54albert54 <126289455+54albert54@user.noreply.github.com>
2023-09-10 01:30:33 -0400

    ready for deploy

--- main.js
+++ main.js
@@ -8,17 +8,19 @@ const titleAlert = document.getElementById('alertTitle')
 const alertButon = document.getElementById('alertButon')
 
 const frutas = ["aguacate","banana","cereza","manzana","piña","sandia","aguacate","banana","cereza","manzana","piña","sandia"]
+// const letras = ['letraa','letrab','letrac','letrad','letrae','letraf','letrag','letrah','letrai','letraj','letrak','letral','letram','letran','letrao','letrap','letraq',
+// 'letrar','letras','letrat','letrau','letrav','letraw','letrax','letray','letraz','letraa','letrab','letrac','letrad','letrae','letraf','letrag','letrah','letrai','letraj','letrak','letral','letram','letran','letrao','letrap','letraq',
+// 'letrar','letras','letrat','letrau','letrav','letraw','letrax','letray','letraz',]
+
 const letras = ['letraa','letrab','letrac','letrad','letrae','letraf','letrag','letrah','letrai','letraj','letrak','letral','letram','letran','letrao','letrap','letraq',
-'letrar','letras','letrat','letrau','letrav','letraw','letrax','letray','letraz','letraa','letrab','letrac','letrad','letrae','letraf','letrag','letrah','letrai','letraj','letrak','letral','letram','letran','letrao','letrap','letraq',
-'letrar','letras','letrat','letrau','letrav','letraw','letrax','letray','letraz',]
+'letrar','letras','letrat','letrau','letrav','letraw','letrax','letray','letraz']
 let cards ;
 let lastShow;
 let currentShow;
 let lastCard;
 let oldId;
 let pares = 0
-let limitPeers = letras.length
-mensjeG.innerHTML = `Pares ${pares} de ${limitPeers}`
+let limitPeers;
 const cardTemplate = `
 <div class="card-front"> </div>
 <div class="card-back"></div>
@@ -26,11 +28,16 @@ const cardTemplate = `
 
 
 let machArrayObject ={}
+
+
 function shuffleArray(array) {
+  
   for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
+      
   }
+  return array
 }
 
 
@@ -51,7 +58,7 @@ setTimeout(()=>{
   mainContainer.innerHTML = ""
   for (let i = 0 ; i < array.length ;i++){
     machArrayObject={...machArrayObject,
-    [i]:frutas[i]}
+    [i]:array[i]}
     const div = document.createElement('div');
     div.classList.add('card')
     div.id=i
@@ -74,6 +81,8 @@ setTimeout(() => {
       selectCart.classList.add('cardVolteada')
       let newId =selectCart.id //esto es para evitar que si tocas dos veces la misma carta el juego te detecte masch
       currentShow = machArrayObject[selectCart.id];
+      //console.log(machArrayObject);
+      
       if (oldId !=newId){
       if (lastShow == currentShow){
         pares++
@@ -81,7 +90,8 @@ setTimeout(() => {
         mensaje.innerHTML ='Tenemos masch';
         lastCard.classList.add('win') // se le agrega esto para que no pueda ser volteada si pasa el tiempo
         lastCard.classList.add('cardVolteada')
-        if (pares == 6){
+
+        if (pares == limitPeers){
           alertShow.classList.remove('hiden');
           alertTitle.innerHTML = "Felicidades";
           alertButon.innerHTML = "Volver a Jugar";
@@ -105,15 +115,32 @@ setTimeout(() => {
   )
 }, 3000);
 }
-const startGame = ()=>{
+const startGame = (mode)=>{
+  console.log(mode);
+  
   pares= 0;
   mensjeG.innerHTML = `Pares ${pares} de ${limitPeers}`
   mensaje.innerHTML ='';
   mainContainer.innerHTML = " "
   alertShow.classList.add('hiden')
-  shuffleArray(letras);
-  pintarCartas(letras)  
+ const newArray = shuffleArray(letras);
+ const a = newArray.slice(0,mode) //6combos 20 9combos 17   12combos 14
+ const newA = []
+ for(let i = 0;i < a.length ; i++){
+  newA.push(a[i])
+  newA.push(a[i])
+  
+ }
+ 
+ shuffleArray(newA);
+ limitPeers= newA.length/2
+ mensjeG.innerHTML = `Pares ${pares} de ${limitPeers}`
+  pintarCartas(newA)  
 }
+
+
+
+
 
 
 
